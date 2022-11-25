d024786c2904a8730b6871aec2c15e5847288350
Nour Elden Alaa <nourelden515@gmail.com>
2022-11-25 22:45:44 +0200

    #implementing insertReminderAndGetById(). -Using runBlocking to test Asynchronous (Coroutines)/its the same as runBlocking but used in tests. -use assertThat and 'is' to check if the values of retrieved data is the same as stored data.

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -5,10 +5,15 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
@@ -36,5 +41,27 @@ class RemindersDaoTest {
 
     @After
     fun closeDb() = database.close()
+
+    @Test
+    fun insertReminderAndGetById() = runBlockingTest {
+        val testReminder = ReminderDTO(
+            title = "reminder title",
+            description = "description",
+            location = "Cairo",
+            latitude = 30.033333,
+            longitude = 31.233334
+        )
+        database.reminderDao().saveReminder(testReminder)
+
+        val loaded = database.reminderDao().getReminderById(testReminder.id)
+
+        MatcherAssert.assertThat(loaded as ReminderDTO, CoreMatchers.notNullValue())
+        MatcherAssert.assertThat(loaded.id, CoreMatchers.`is`(testReminder.id))
+        MatcherAssert.assertThat(loaded.title, CoreMatchers.`is`(testReminder.title))
+        MatcherAssert.assertThat(loaded.description, CoreMatchers.`is`(testReminder.description))
+        MatcherAssert.assertThat(loaded.latitude, CoreMatchers.`is`(testReminder.latitude))
+        MatcherAssert.assertThat(loaded.longitude, CoreMatchers.`is`(testReminder.longitude))
+        MatcherAssert.assertThat(loaded.location, CoreMatchers.`is`(testReminder.location))
+    }
 
 }
