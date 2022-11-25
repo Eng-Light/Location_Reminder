51f77503f3f80238bf47e1dc79602d90d1d3536f
Nour Elden Alaa <nourelden515@gmail.com>
2022-11-25 11:40:35 +0200

    #Implementing zoom to the user location after taking his permission 1) Request Permissions: -get object from ActivityResultLauncher to request ACCESS_FINE_LOCATION and ACCESS_COARSE_LOCATION. -implement isUserLocationEnabled() to Check if the location permissions is granted and request it if not using the requestPermissionLauncher. -using shouldShowRequestPermissionRationale() as a best practice for asking user permissions. -enable MyLocationEnabled Button. -enable uiSettings. 2)GetLocation: -implement getLocation() function to Get User's last location if exist and if not -> call getCurrentLocation. -using object from FusedLocationProviderClient to receiving location updates. -implement getCurrentLocation() function to get current location using fusedLocationProviderClient.
    #Note that the functionality of this fragment doesn't require enable GPS from settings as the user can simply choose location on the map.
     So its optional to the user.

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,17 +1,30 @@
 package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
 
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Location
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.lifecycle.Lifecycle
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.CancellationTokenSource
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
@@ -25,7 +38,15 @@ class SelectLocationFragment : BaseFragment(), MenuProvider, OnMapReadyCallback 
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
 
+    // FusedLocationProviderClient - Main class for receiving location updates.
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private var lat: Double = 0.0
+    private var long: Double = 0.0
+    private var title = ""
+
     private lateinit var map: GoogleMap
+    private var requestPermissionLauncher = registerCallBack()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -39,7 +60,7 @@ class SelectLocationFragment : BaseFragment(), MenuProvider, OnMapReadyCallback 
         setupMap()
 
 //        add the map setup implementation
-//        TODO: zoom to the user location after taking his permission
+//        zoom to the user location after taking his permission
 //        TODO: add style to the map
 //        TODO: put a marker to location that the user selected
 
@@ -70,12 +91,158 @@ class SelectLocationFragment : BaseFragment(), MenuProvider, OnMapReadyCallback 
         map.uiSettings.isZoomControlsEnabled = true
         map.uiSettings.isCompassEnabled = true
         map.uiSettings.isMyLocationButtonEnabled = true
+        getLocation()
     }
 
     private fun onLocationSelected() {
         //        TODO: When the user confirms on the selected location,
         //         send back the selected location details to the view model
         //         and navigate back to the previous fragment to save the reminder and add the geofence
+    }
+
+    /**
+     * Register callback to request location permissions
+     */
+    private fun registerCallBack(): ActivityResultLauncher<Array<String>> {
+        val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    //Enable MyLocationButton.
+                    isUserLocationEnabled()
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    Toast.makeText(
+                        context, "Only approximate location access granted.", Toast.LENGTH_LONG
+                    ).show()
+                }
+                else -> {
+                    Toast.makeText(
+                        context, "Location permission was not granted.", Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+        return requestPermissionLauncher
+    }
+
+    /**
+     * Shows dialog to inform the user why we need those permissions.
+     */
+    private fun showRationaleDialog(title: String, message: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        builder.setTitle(title).setMessage(message).setPositiveButton("Ok") { _, _ ->
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+        builder.create().show()
+    }
+
+    /**
+     * Check if the location permissions is granted and request it if not.
+     */
+    private fun isUserLocationEnabled(): Boolean {
+        var result = false
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // This condition only becomes true if the user has denied the permission previously
+            if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) && shouldShowRequestPermissionRationale(
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            ) {
+                showRationaleDialog(
+                    getString(R.string.rationale_title), getString(R.string.rationale_desc)
+                )
+            } else {
+                requestPermissionLauncher.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                )
+            }
+        } else {
+            Toast.makeText(
+                context, "Location permissions is granted.", Toast.LENGTH_LONG
+            ).show()
+            map.isMyLocationEnabled = true
+            map.uiSettings.isMyLocationButtonEnabled = true
+            result = true
+        }
+        return result
+    }
+
+    /**
+     * Get User's last location if exist and if not -> request current location.
+     */
+    @Suppress("MissingPermission")
+    private fun getLocation() {
+        if (isUserLocationEnabled()) {
+            fusedLocationProviderClient.lastLocation.addOnCompleteListener { resultLocation ->
+                if (resultLocation.isSuccessful) {
+                    resultLocation.run {
+                        if (this.result != null) {
+                            focusOnCurrentLocation(this.result)
+                        } else {
+                            showRationaleDialog(
+                                "Location is Disabled",
+                                "Please Enable Your Location Settings"
+                            )
+                        }
+                    }
+                } else {
+                    getCurrentLocation()
+                }
+            }
+        }
+    }
+
+    /**
+     * Get current location.
+     */
+    @Suppress("MissingPermission")
+    private fun getCurrentLocation() {
+        val priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        val cancellationTokenSource = CancellationTokenSource()
+
+        fusedLocationProviderClient.getCurrentLocation(priority, cancellationTokenSource.token)
+            .addOnSuccessListener { location ->
+                if (location != null) {
+                    focusOnCurrentLocation(location)
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Please Enable your Location Settings",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+    }
+
+    private fun focusOnCurrentLocation(location: Location) {
+        val zoomLevel = 15f
+        val latLong = LatLng(location.latitude, location.longitude)
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLong, zoomLevel))
+        map.addMarker(
+            MarkerOptions().position(latLong).title(getString(R.string.my_location_pin))
+        )?.showInfoWindow()
+        map.addCircle(
+            CircleOptions().center(latLong).radius(150.0).strokeColor(Color.argb(255, 255, 0, 0))
+                .fillColor(Color.argb(64, 255, 0, 0)).strokeWidth(4F)
+
+        )
+        lat = latLong.latitude
+        long = latLong.longitude
+        title = getString(R.string.dropped_pin)
     }
 
     override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
