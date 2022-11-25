3c0da7865534df959b2e5212f2d65c5c83f99f80
Nour Elden Alaa <nourelden515@gmail.com>
2022-11-25 19:03:53 +0200

    #Setup SaveReminderViewModelTest.kt -get instance from InstantTaskExecutorRule() to execute each task synchronously using Architecture Components. -get instance from SaveReminderViewModel as a subject under test. -get instance from FakeDataSource to be injected to the viewModel. - Set the main coroutines dispatcher roles for unit testing. -initializing the Fake Repository.

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,49 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.rule.MainCoroutineRule
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
 
+    //provide testing to the SaveReminderViewModel and its live data objects
 
-    //TODO: provide testing to the SaveReminderView and its live data objects
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
 
+    // Set the main coroutines dispatcher for unit testing.
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    // Use a fake repository to be injected into the viewModel
+    private lateinit var fakeRepo: FakeDataSource
+
+    // Subject under test
+    private lateinit var saveReminderViewModel: SaveReminderViewModel
+
+    //initializing the fake repo
+    @Before
+    fun setupSaveReminderViewModel() {
+        stopKoin()
+        fakeRepo = FakeDataSource()
+        saveReminderViewModel = SaveReminderViewModel(
+            ApplicationProvider.getApplicationContext(),
+            fakeRepo
+        )
+        //reset fakeDataBase
+        runBlocking { fakeRepo.deleteAllReminders() }
+    }
 
 }
