9803885c372924f01f13aa9fa93c6cd43c2a2538
Nour Elden Alaa <nourelden515@gmail.com>
2022-11-25 18:31:47 +0200

    #Setup RemindersListViewModelTest.kt -get instance from InstantTaskExecutorRule() to execute each task synchronously using Architecture Components. -get instance from RemindersListViewModel as a subject under test. -get instance from FakeDataSource to be injected to the viewModel. - Set the main coroutines dispatcher roles for unit testing. -initializing the Fake Repository.

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,47 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.rule.MainCoroutineRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    // Subject under test
+    private lateinit var reminderViewModel: RemindersListViewModel
+
+    // Use a fake repository to be injected to the viewModel
+    private lateinit var fakeRepo: FakeDataSource
+
+    // Set the main coroutines dispatcher roles for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    //initializing the fake repo
+    @Before
+    fun setupRemindersListViewModel() {
+        stopKoin()
+
+        fakeRepo = FakeDataSource()
+        reminderViewModel = RemindersListViewModel(
+            ApplicationProvider.getApplicationContext(),
+            fakeRepo
+        )
+    }
 
 }
